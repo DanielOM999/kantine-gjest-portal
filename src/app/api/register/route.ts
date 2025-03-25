be58587514b34d13badde46378843b448c2a36de
DanielOM999 <142886750+DanielOM999@users.noreply.github.com>
2025-03-25 12:25:22 +0100

    fixed mentality not there error

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -1,7 +1,7 @@
 import { type NextRequest, NextResponse } from "next/server"
 import { db } from "@/src/lib/db"
 
-const baseUrl = 'http://192.168.1.100';
+const baseUrl = 'http://192.168.1.3:3000';
 
 const getClientIp = (request: NextRequest): string => {
   // For reverse proxy setups
